1ee8c47da8a5d1e7b22bb3265a794309bdcbd1f4
Luko van der Maas <me@luko.dev>
2023-08-30 18:51:23 +0200

    update and fix nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "ElPi test";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
