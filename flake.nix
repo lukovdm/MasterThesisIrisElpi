a82b057ef1a59048bd1ddfdca518629783a9eab6
Luko van der Maas <me@luko.dev>
2023-08-30 18:46:07 +0200

    initial commit with flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "ElPi test";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell
+          {
+            nativeBuildInputs = [ pkgs.opam pkgs.gnumake pkgs.gmp pkgs.coqPackages.iris pkgs.coqPackages.coq-elpi ];
+            shellHook = ''
+              eval $(opam env)
+            '';
+          };
+      });
+}
