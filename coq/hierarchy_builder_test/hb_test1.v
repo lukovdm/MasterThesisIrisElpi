f245c2de4fd2e1d4bceaec7aa29882bd28a57348
Luko van der Maas <me@luko.dev>
2023-09-01 13:20:26 +0200

    it has some syntax errors at the end, that I don't get

--- coq/hierarchy_builder_test/hb_test1.v
+++ coq/hierarchy_builder_test/hb_test1.v
@@ -4,30 +4,29 @@ From HB Require Import structures.
 HB.mixin Record RA_of_TYPE M := {
   valid : M -> Prop;
   core : M -> option M;
-  opp : M -> M -> M;
+  op : M -> M -> M;
   lte : M -> M -> Prop;
-  lteINCL : forall a b, exists c, b = opp a c;
-  oppA : associative opp;
-  oppC : commutative opp;
-  coreID : forall a, (exists a', core a = Some a' /\ opp a' a = a);
+  lteINCL : forall a b, exists c, b = op a c;
+  opA : associative op;
+  opC : commutative op;
+  coreID : forall a, (exists a', core a = Some a' /\ op a' a = a);
   coreIDEM : forall a, (exists a', core a = Some a' /\ core a' = Some a');
   coreMONO : forall a b, 
     (exists a', core a = Some a' /\ lte a' b -> 
       (exists b', core b = Some b' /\ lte a' b')
     );
-  validOPP : forall a b, valid (opp a b) -> valid a;
+  validOP : forall a b, valid (op a b) -> valid a;
 }.
 HB.structure Definition RA := { M of RA_of_TYPE M }.
 
 (* This does not work with the error, (equ n) is not a Type, but I don't know why *)
-(*
-HB.mixin Record OFE_of_TYPE T := {
+Fail HB.mixin Record OFE_of_TYPE T := {
   equ : nat -> T -> T -> Prop;
   equREFL : forall n, reflexive (equ n);
   equTRANS : forall n, transitive (equ n);
   equSYMM : forall n, symmetric (equ n);
   equMONO : forall n m, n >= m -> inclusion (equ n) (equ m);
-}. *)
+}.
 
 HB.mixin Record OFE_of_TYPE T := {
   equ : nat -> T -> T -> Prop;
@@ -123,29 +122,85 @@ Definition is_chain {T : OFE.type} (c : nat -> T) :=
 
 (* This does not work since we can't define lim like this, 
    however, I also don't know how to do it otherwise. *)
-(* HB.mixin Record COFE_of_OFE T of OFE T := {
+Fail HB.mixin Record COFE_of_OFE T of OFE T := {
     lim : forall c : nat -> T, is_chain T c -> c -> T;
     limCOMPL : forall n c, equ n (lim c _) (c n);
-}. *)
+}. 
 
 HB.mixin Record CAMERA_of_OFE M of OFE M := {
   valid : M -> Prop; (* Should be SProp, but don't understand that yet *)
-  (* validNE : non_expansive valid; *) 
+  (* validNE : non_expansive valid; *)
   (* does not work since Prop is not OFE, but maybe SProp is? *)
   core : M -> option M;
   coreNE : non_expansive core;
-  opp : M -> M -> M;
+  op : M -> M -> M;
+  (* opNE : non_expansive op; *)
+  (* Don't yet know how to define non_expansive for M -> M -> M *)
   lte : M -> M -> Prop;
-  lteINCL : forall a b, exists c, b = opp a c;
-  lteINCLN : forall a b n, exists c, equ n b (opp a c);
-  oppA : associative opp;
-  oppC : commutative opp;
-  coreID : forall a, (exists a', core a = Some a' /\ opp a' a = a);
+  lteINCL : forall a b, exists c, b = op a c;
+  lteINCLN : forall a b n, exists c, equ n b (op a c);
+  opA : associative op;
+  opC : commutative op;
+  coreID : forall a, (exists a', core a = Some a' /\ op a' a = a);
   coreIDEM : forall a, (exists a', core a = Some a' /\ core a' = Some a');
   coreMONO : forall a b, 
     (exists a', core a = Some a' /\ lte a' b -> 
       (exists b', core b = Some b' /\ lte a' b')
     );
-  validOPP : forall a b, valid (opp a b) -> valid a;
-  EXTEND : forall n a b1 b2, (* n is element of valid a /\ *) equ n a (opp b1 b2) -> exists c1 c2, equ n a (opp c1 c2) /\ equ n c1 b1 /\ equ n c2 b2;
+  validop : forall a b, valid (op a b) -> valid a;
+  EXTEND : forall n a b1 b2, (* n is element of valid a /\ *) 
+    equ n a (op b1 b2) -> 
+      exists c1 c2, equ n a (op c1 c2) /\ equ n c1 b1 /\ equ n c2 b2;
 }.
+HB.structure Definition CAMERA := { M of OFE M & CAMERA_of_OFE M }.
+
+
+(* I have no clue how to use non_expansive here, 
+   as we haven't proven yet that this record is also an ofe.
+   That only happens in the to_OFE definition below.
+   You might also be able to prove either coreNE or one of the others
+   from the ofe axioms, but haven't figured that out yet *)
+  Fail HB.factory Record CAMERA_of_RA M of RA M := {
+    equ : nat -> M -> M -> Prop;
+    equREFL : forall n x, equ n x x;
+    equTRANS : forall n x y z, equ n x y -> equ n y z -> equ n x z;
+    equSYMM : forall n x y, equ n x y -> equ n y x;
+    equMONO : forall n m, n >= m -> forall x y, equ n x y -> equ m x y;
+    equLIMIT : forall x y, x = y <-> forall n, equ n x y;
+
+    coreNE : non_expansive core;
+    lteINCLN : forall a b n, exists c, equ n b (op a c);
+    EXTEND : forall n a b1 b2, (* n is element of valid a /\ *) 
+      equ n a (op b1 b2) -> 
+        exists c1 c2, equ n a (op c1 c2) /\ equ n c1 b1 /\ equ n c2 b2;
+  }.
+
+  Fail HB.builders Context M (m : CAMERA_of_RA M).
+
+    Fail HB.instance
+    Definition to_OFE :=
+      OFE_of_TYPE.Build M equ equREFL equTRANS equSYMM equMONO equLIMIT.
+
+    Fail HB.instance
+    Definition to_CAMERA_of_OFE :=
+      CAMERA_of_OFE.Build M valid core coreNE op lte lteINCL lteINCLN 
+        opA opC coreID coreIDEM coreMONO validOP EXTEND.
+  Fail HB.end.
+(* End of part I have questions about *)
+
+HB.graph "iris_hierarchy.dot".
+
+Declare Scope hb_scope.
+Delimit Scope hb_scope with G.
+Local Open Scope hb_scope.
+Infix "." := (@op _) (at level 50, left associativity) : hb_scope.
+
+Section camera.
+Variable (M : CAMERA.type).
+(* I have no clue why this fails, it feels like it is 
+   just giving random errors now *)
+Implicit Types x y z : M.
+
+(* op *)
+Fail Lemma opA x y z : @op M x (@op M y z) = @op M (@op M x y) z.
+
